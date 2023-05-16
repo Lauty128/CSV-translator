7b4774924394b2562b14fe9db4d60305d66eee35
Lauty128 <lautarosilverii@gmail.com>
2023-05-16 14:24:44 -0300

    use of ads only for some pages

--- src/controllers/pages.controller.js
+++ src/controllers/pages.controller.js
@@ -1,15 +1,15 @@
 export const homePage = (req,res) => {
-    res.render('pages/index')
+    res.render('pages/index', { ads:true })
 }
 
 export const createPage = (req,res) => {
-    res.render('pages/table')
+    res.render('pages/table', { ads:true })
 }
 
 export const submitPage = (req,res) => {
-    res.render('pages/submit')
+    res.render('pages/submit', { ads:false })
 }
 
 export const intervalsPage = (req,res) => {
-    res.render('pages/intervals')
+    res.render('pages/intervals', { ads:true })
 }
