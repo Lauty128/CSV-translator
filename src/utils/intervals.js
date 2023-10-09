07bec19094dad47e4c9827031889f14a1173f32f
Lauty128 <lautarosilverii@gmail.com>
2023-10-09 13:47:15 -0300

    Interval class optimized and some errors fixed

--- src/utils/intervals.js
+++ src/utils/intervals.js
@@ -23,28 +23,47 @@ export class Interval{
 
     getTable(){
         //------- Set variables
-        let number = this.min
-        const amplitude = this.amplitude
-        let index = 1
+            // Number is equal to minimum number of the all interval
+            let number = this.min
+            const amplitude = this.amplitude
+            let index = 1
+        
         //------- Create table
-        const table = new Array(this.number_of_classes + 2)
-        table[0] = ['Variables', 'Frecuencia']
-        table[table.length - 1] = ['Total', this.numbers.length]
+            // Table is the classes number plus 2, because the table need space for the header and footer
+            const table = new Array(this.number_of_classes + 2)
         
+            // Define value of the header and footer
+            table[0] = ['Variables', 'Frecuencia']
+            table[table.length - 1] = ['Total', this.numbers.length]
+        
+        //------- This loop is reponsible for finding the frecuency of each interval
         while(index <= this.number_of_classes){
+            // The number begin in the minimum and it adds the amplitud in each turn
             number += amplitude
+
+            // These indicates the start and end of each interval
+            const start = number - amplitude;
+            const end = index == this.number_of_classes
+                        ? this.max
+                        : (number - 1);
+
             table[index] = new Array(2)
-            table[index][0] = `${number - amplitude}-${number}`
+            // table[Index][0] represents the search interval
+            table[index][0] = start + '-' + end
+
+            // table[Index][1] represents the total of number in the interval
             table[index][1] = 0
-            this.numbers.forEach(element => table[index][1] += this.getNumber(element >= (number - amplitude)) && element <= number);
-        
+            this.numbers.forEach(element =>
+                table[index][1] += this.getNumber(element, start, end))
+
+            // Add one to the index
             index++
         }
         this.table = table
     }
 
-    getNumber(condition){
-        return (condition) ? 1 : 0
+    getNumber(number, startInterval, endInterval){
+        return ((number >= startInterval) && (number <= endInterval)) ? 1 : 0
     }
 
     
